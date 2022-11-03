bdb5dbe854f8965ca4427b981fa9a2251702cbef
Alexander <aromanov999@list.ru>
2022-11-03 17:40:24 +0400

    second commit.

---
+++ Task4/Task4.cs
@@ -0,0 +1,15 @@
+Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numB = int.Parse(Console.ReadLine());
+if ((numA >= numB) && (numA >= numC)) {
+    Console.WriteLine(numA);
+}
+if ((numB >= numA) && (numB >= numC)) {
+    Console.WriteLine(numA);
+}
+if ((numC >= numA) && (numC >= numB)) {
+    Console.WriteLine(numA);
+}
